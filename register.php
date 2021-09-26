e7947a73a86d3486b657c58a09b607101a2cbd77
chungnn <chungnguyen2010@gmail.com>
2021-09-26 12:04:24 +0700

    init project

---
+++ register.php
@@ -0,0 +1,84 @@
+<?php
+	include("db_connect.php");
+	session_start();
+	
+	if(isset($_POST['submit']))
+	{	
+		$name = $_POST['name'];
+		$name = stripslashes($name);
+		$name = addslashes($name);
+
+		$email = $_POST['email'];
+		$email = stripslashes($email);
+		$email = addslashes($email);
+
+		$password = $_POST['password'];
+		$password = stripslashes($password);
+		$password = addslashes($password);
+		$password = md5($password);
+
+		
+		$str="SELECT username from users WHERE username='$name'";
+		$result=mysqli_query($conn, $str);
+		
+		if((mysqli_num_rows($result))>0)	
+		{
+            echo "<center><h3><script>alert('Sorry.. This email is already registered !!');</script></h3></center>";
+            header("refresh:0;url=login.php");
+        }
+		else
+		{
+            $str="insert into users (name, user_type, username, password, status) VALUES('$name', 3, '$name', '$password', 1)";
+			
+			if((mysqli_query($conn, $str)))	
+				echo "<center><h3><script>alert('Congrats.. You have successfully registered !!');</script></h3></center>";
+			header('location: index.php?q=1');
+		}
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>Register | Online Quiz System</title>
+		<?php include('header.php') ?>
+	</head>
+
+	<body>
+		<section class="login first grey">
+			<div class="container">
+				<div class="box-wrapper">				
+					<div class="box box-border">
+						<div class="box-body">
+							<center> <h5 style="font-family: Noto Sans;">Register to </h5><h4 style="font-family: Noto Sans;">Online Quiz System</h4></center><br>
+							<form method="post" action="register.php" enctype="multipart/form-data">
+                                <div class="form-group">
+									<label>Enter Your Username:</label>
+									<input type="text" name="name" class="form-control" required />
+								</div>
+								<div class="form-group">
+									<label>Enter Your Email Id:</label>
+									<input type="email" name="email" class="form-control" required />
+								</div>
+								<div class="form-group">
+									<label>Enter Your Password:</label>
+									<input type="password" name="password" class="form-control" required />
+                                </div>
+								
+								<div class="form-group text-right">
+									<button class="btn btn-primary btn-block" name="submit">Register</button>
+								</div>
+								<div class="form-group text-center">
+									<span class="text-muted">Already have an account! </span> <a href="login.php">Login </a> Here..
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</body>
+</html>
