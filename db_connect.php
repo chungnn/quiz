bf45574b8125a9290cd0e922070056e73d0ebc8f
chungnn <chungnguyen2010@gmail.com>
2021-09-26 16:21:12 +0700

    refactor code file db_connect.php to use .env file

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,16 @@
 <?php
-$conn= new mysqli('localhost','root','','quiz') or die("Could not connect to mysql".mysqli_error($con));
+
+define('DS', DIRECTORY_SEPARATOR);
+define('APP_DIR', dirname(__FILE__));
+require_once APP_DIR . DS . "vendor" . DS . "autoload.php";
+
+$dotenv = Dotenv\Dotenv::createImmutable(APP_DIR);
+$dotenv->load();
+
+$host = $_SERVER["DB_HOST"];
+$username = $_SERVER["DB_USERNAME"];
+$password = $_SERVER["DB_PASSWORD"];
+$dbname = $_SERVER["DB_NAME"];
+
+$conn = new mysqli($host, $username, $password, $dbname) or die("Could not connect to mysql".mysqli_error($con));
 
